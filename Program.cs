48460f7c8811bfe35b0ca2f768613cdc148c18ac
GooFy <GooFy@DESKTOP-VNCBOFR>
2022-10-20 17:15:49 +0330

    An OOP based program in C# console

--- Program.cs
+++ Program.cs
@@ -1,58 +1,68 @@
-﻿
-
-// Declare variables and then initialize to zero.
-int num1 = 0; int num2 = 0;
-bool flag = true;
-string answer = "";
-// Display title as the C# console calculator app.
-Console.WriteLine("Console Calculator in C#\r");
-Console.WriteLine("------------------------\n");
-
-
-while (flag)
+﻿class Program
 {
-    // Ask the user to type the first number.
-    Console.WriteLine("Type a number, and then press Enter");
-    num1 = Convert.ToInt32(Console.ReadLine());
-
-    // Ask the user to type the second number.
-    Console.WriteLine("Type another number, and then press Enter");
-    num2 = Convert.ToInt32(Console.ReadLine());
-
-    // Ask the user to choose an option.
-    Console.WriteLine("Choose an option from the following list:");
-    Console.WriteLine("\ta - Add");
-    Console.WriteLine("\ts - Subtract");
-    Console.WriteLine("\tm - Multiply");
-    Console.WriteLine("\td - Divide");
-    Console.WriteLine("\te - Exit");
-    Console.Write("Your option? ");
-    // Use a switch statement to do the math.
-    switch (Console.ReadLine())
+    static void Main(string[] args)
     {
-        case "a":
-            Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
-            break;
-        case "s":
-            Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
-            break;
-        case "m":
-            Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
-            break;
-        case "d":
-            Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
-            break;
-        case "e":
-            Environment.Exit(1);
-            break;
-    }
+        bool endApp = false;
+        Console.WriteLine("Console Calculator in C#\r");
+        Console.WriteLine("------------------------\n");
 
-    Console.WriteLine("Do you still want to continue ?   yes/no");
-    answer = Console.ReadLine();
-    if (answer=="yes") 
-        flag = true;   
-    else if(answer == "no"){
-        flag = false;
-        Environment.Exit(1);
-    };
+        while (!endApp)
+        {
+            string numInput1 = "";
+            string numInput2 = "";
+            double result = 0;
+
+            Console.Write("Type a number, and then press Enter: ");
+            numInput1 = Console.ReadLine();
+
+            double cleanNum1 = 0;
+            while (!double.TryParse(numInput1, out cleanNum1))
+            {
+                Console.Write("This is not valid input. Please enter an integer value: ");
+                numInput1 = Console.ReadLine();
+            }
+
+            Console.Write("Type another number, and then press Enter: ");
+            numInput2 = Console.ReadLine();
+
+            double cleanNum2 = 0;
+            while (!double.TryParse(numInput2, out cleanNum2))
+            {
+                Console.Write("This is not valid input. Please enter an integer value: ");
+                numInput2 = Console.ReadLine();
+            }
+
+            Console.WriteLine("Choose an operator from the following list:");
+            Console.WriteLine("\ta - Add");
+            Console.WriteLine("\ts - Subtract");
+            Console.WriteLine("\tm - Multiply");
+            Console.WriteLine("\td - Divide");
+            Console.Write("Your option? ");
+
+            string op = Console.ReadLine();
+
+            try
+            {
+                result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
+              
+                if (double.IsNaN(result))
+                {
+                    Console.WriteLine("This operation will result in a mathematical error.\n");
+                }
+                else Console.WriteLine("Your result: {0:0.##}\n", result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+            }
+
+            Console.WriteLine("------------------------\n");
+
+            Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+            if (Console.ReadLine() == "n") endApp = true;
+
+            Console.WriteLine("\n");
+        }
+        return;
+    }
 }
